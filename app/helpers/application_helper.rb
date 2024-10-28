b0283eb2578004890e99af35cc78412b92b23be7
Arif Hossain <hamid_kawser@hotmail.be>
2024-10-28 13:17:14 +0600

    added style to campaign

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def active_link(url_path)
     "active" if request.path == url_path
   end
+
+  def number_to_k(number)
+    number_to_human(number, format: "%n%u", units: { thousand: "K" })
+  end
 end
