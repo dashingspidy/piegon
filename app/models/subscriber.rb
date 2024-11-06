1f0f38b792aa18c3d534b67487164dedca6b8209
Arif Hossain <hamid_kawser@hotmail.be>
2024-11-06 08:57:31 +0600

    subscriber unique to campaign

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -1,3 +1,15 @@
 class Subscriber < ApplicationRecord
   belongs_to :campaign
+
+  validates :email, presence: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: "Not a valid email address" }
+  validate :unique_email_by_campaign
+
+  private
+
+  def unique_email_by_campaign
+    if campaign.subscribers.where.not(id: id).exists?(email: email)
+      errors.add(:email, "Email already subscribed.")
+    end
+  end
 end
