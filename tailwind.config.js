facf0b2efd01f6bac7fac3ab0a80fc0413e4e84f
Arif Hossain <hamid_kawser@hotmail.be>
2024-12-11 10:22:16 +0600

    new font, gem update

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,11 +15,16 @@ module.exports = {
   darkMode: 'false',
   theme: {
     colors: {
-        primary: colors.slate
+        primary: colors.slate,
+        "prim": "#E1D6DC",
+        "link-water": "#C9CCDC",
+        "lavender-gray": "#C0C8DB",
+        "ship-cove": "#838BAD",
+        "chambray": "#464e7f"
     },
     fontFamily: {
-      'body': ['Geist'],
-      'sans': ['Geist']
+      'sans': ['Geist'],
+      'logo': ['Borel'],
     },
     extend: {
       animation: {
