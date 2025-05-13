b94c43699946f8fa4144dba710edc7a6166f93fa
Arif Hossain <hamid_kawser@hotmail.be>
2025-05-13 12:45:34 +0200

    bug fixing and testing

--- config/initializers/turnstiled.rb
+++ config/initializers/turnstiled.rb
@@ -1,2 +1,4 @@
-Turnstiled.site_key = Rails.application.credentials.dig(:turnstiled, :site_key)
-Turnstiled.site_secret = Rails.application.credentials.dig(:turnstiled, :site_secret)
+Rails.application.config.to_prepare do
+  Turnstiled.site_key = Rails.application.credentials.dig(:turnstiled, :site_key)
+  Turnstiled.site_secret = Rails.application.credentials.dig(:turnstiled, :site_secret)
+end
